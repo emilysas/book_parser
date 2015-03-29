e9182f6d6c0100de399b251ad4cba506f8f7760f
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-29 21:46:03 +0100

    adding first test - can add a word to dictionary

---
+++ book_parser/Test.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace book_parser
+{
+	[TestFixture ()]
+	public class RefDictionary
+	{
+		Dictionary<string, int> dictionary;
+
+		[SetUp]
+		public void Init ()
+		{
+			dictionary = new Dictionary<string, int> ();
+		}
+
+		[Test ()]
+		public void WillAddNewWordToDictionary ()
+		{
+			Assert.AreEqual("Hello: 1", dictionary.AddWord("Hello"));
+		}
+	}
+}
+
