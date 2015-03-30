0aa07798260e0fb0d3e8b5ca9e424ba7d0349583
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-30 01:42:05 +0100

    writing new test - text parser will create an array of strings

--- book_parser/textparsertest.cs
+++ book_parser/textparsertest.cs
@@ -8,17 +8,58 @@ namespace book_parser
 	public class TextParserTest
 	{
 		TextParser textparser;
+		string[] wordarray;
 
 		[SetUp]
 		public void Init ()
 		{
 			textparser = new TextParser ();
+			wordarray = new string[] {
+				"and",
+				"then",
+				"something",
+				"went",
+				"bump",
+				"How",
+				"that",
+				"bump",
+				"made",
+				"us",
+				"jump",
+				"we",
+				"looked",
+				"then",
+				"we",
+				"saw",
+				"him",
+				"step",
+				"in",
+				"on",
+				"the",
+				"mat",
+				"we",
+				"looked",
+				"and",
+				"we",
+				"saw",
+				"him",
+				"the",
+				"cat",
+				"in",
+				"the",
+				"hat"
+			};
 		}
 
 		[Test ()]
 		public void WillParseTextFileAndReturnString ()
 		{
 			Assert.AreEqual("hello world", textparser.GetWords("../../testfiles/ExampleTextFile.txt"));
+		}
+
+		public void WillCreateArrayOfStrings ()
+		{
+			Assert.AreEqual(wordarray, textparser.CreateArray("And then something went BUMP! How that bump made us jump! We looked! Then we saw him step in on the mat! We looked! And we saw him! The Cat in the Hat!"));
 		}
 
 	}
