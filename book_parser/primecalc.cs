242e785b82102a90e317af21c9211b4da605f6b2
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-30 02:30:58 +0100

    passing test - can calculate prime numbers

--- book_parser/primecalc.cs
+++ book_parser/primecalc.cs
@@ -2,11 +2,37 @@
 
 namespace book_parser
 {
-	public class primecalc
+	public class PrimeNumberCalculator
 	{
-		public primecalc ()
+		public PrimeNumberCalculator ()
 		{
 		}
+
+		public bool IsAPrimeNumber(ulong n)
+		{
+			if (n <=3)
+				return n > 1;
+			if (IsDivisibleByTwoOrThree (n))
+				return false;
+			return IsDivisibleByOtherNumbers (n);
+		}
+
+		private bool IsDivisibleByTwoOrThree(ulong n)
+		{
+			return n % 2 == 0 || n % 3 == 0;
+		}
+
+		private bool IsDivisibleByOtherNumbers(ulong n)
+		{
+			for (ulong i = 5; i * i <= n; i += 6)
+			{
+				if (n % i == 0 || n % (i + 2) == 0)
+					return false;
+			}
+			return true;
+		}
+
+
 	}
 }
 
