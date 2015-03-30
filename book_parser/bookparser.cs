f32342e44f7957bbb0d82aeba1050c2a8b125e57
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-30 18:32:53 +0100

    making objects in Program static

--- book_parser/bookparser.cs
+++ book_parser/bookparser.cs
@@ -4,9 +4,34 @@ namespace book_parser
 {
 	public class Program
 	{
+		static TextParser parser;
+		static RefDictionary dictionary;
+		static PrimeNumberCalculator calculator;
+		static Array words;
+
 		public static void Main (string[] args)
 		{
-			
+			parser = new TextParser ();
+			dictionary = new RefDictionary ();
+			calculator = new PrimeNumberCalculator ();
+ 		}
+
+		public static void RunWordsThroughDictionary(string path)
+		{
+			words = parser.GetWordArray(path);
+			foreach (string word in words) {
+				dictionary.UpdateDictionary(word);
+			}
+		}
+
+		public static void GetData()
+		{
+			dictionary.GetContents ();
+		}
+
+		public static void PrintToConsole ()
+		{
+			Console.WriteLine (GetData());
 		}
 	}
 }
--- book_parser/bookparsertest.cs
+++ book_parser/bookparsertest.cs
@@ -7,24 +7,17 @@ namespace book_parser
 	[TestFixture ()]
 	public class BookParserTest
 	{
-		TextParser textparser;
-		RefDictionary dictionary;
-		PrimeNumberCalculator calculator;
-		Array words;
 
 		[SetUp]
 		public void Init ()
 		{
-			textparser = new TextParser ();
-			dictionary = new RefDictionary ();
-			calculator = new PrimeNumberCalculator ();
-			words = textparser.GetWordArray ("../../testfiles/ExampleTextFile.txt");
 		}
 
 		[Test]
 		public void CanParseTextAndIterateThroughToReturnWordsFrequencies()
 		{
-			Assert.Contains("cat: 100", Program.RunWordsThroughDictionary(words));
+			Program.RunWordsThroughDictionary ("../../testfiles/ExampleTextFile.txt");
+			StringAssert.Contains("cat: 100", Program.PrintToConsole ());
 		}
 	}
 }
