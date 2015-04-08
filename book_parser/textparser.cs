7e47af4a6a6ca4c8a58f17bae5a3390aa08a473c
Emily Sas <emily.bronwen.sas@gmail.com>
2015-04-08 14:05:12 +0100

    Making methods private

--- book_parser/textparser.cs
+++ book_parser/textparser.cs
@@ -14,19 +14,19 @@ namespace book_parser
 			return CreateArrayOfWords(wordString);
 		}
 
-		public string GetWords (string path)
+		private string GetWords (string path)
 		{
 			string readText = File.ReadAllText(path);
 			return readText;
 		}
 
-		public string RemoveUnwantedCharacters(string allChars)
+		private string RemoveUnwantedCharacters(string allChars)
 		{
 			string lowerCaseString = allChars.ToLower();
 			return Regex.Replace (lowerCaseString, @"[\W]", " ");
 		}
 
-		public Array CreateArrayOfWords (string stringOfWords)
+		private Array CreateArrayOfWords (string stringOfWords)
 		{
 			string[] words = stringOfWords.Split (new char[0], StringSplitOptions.RemoveEmptyEntries);
 			return words;
